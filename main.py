04d6eccdb5132a959854092730e9af55f44a377d
kushuu <sonikushu007@gmail.com>
2021-11-02 18:54:03 +0530

    pls work heroku

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main():
     dispatcher.add_error_handler(error)
 
     updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=API_KEY)
-    updater.bot.setWebhook('https://shrouded-savannah-23341.herokuapp.com/' + API_KEY)
+    updater.bot.setWebhook('https://telegram-crypto-bot-kush.herokuapp.com/' + API_KEY)
     updater.idle()
 
 
